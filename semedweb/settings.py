05c848fab6cf52f3dd428bb5cbd2373647ce554c
SemedCanaaDosCarajas <oliveirasantostecnology@gmail.com>
2025-05-13 21:54:00 -0300

    Sistema atualizado em 13 de maio de 2025 as 21:53 ajuste no settings

--- semedweb/settings.py
+++ semedweb/settings.py
@@ -101,16 +101,17 @@ WSGI_APPLICATION = 'semedweb.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'siedge_db',
-        'USER': 'siedge_user',
-        'PASSWORD': '@Drik16091985',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PORT": os.getenv("DB_PORT", "3306"),
+        "OPTIONS": {
+            "charset": "utf8mb4",
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+
         },
     }
 }
